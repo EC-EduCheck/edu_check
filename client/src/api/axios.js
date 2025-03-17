37aaedd608e77ee608da9c8bb262d6763398c519
imdla <imdla918@gmail.com>
2025-03-17 22:27:16 +0900

    [EC-12] style: url 수정

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/posts',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
