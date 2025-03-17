c0173eb25d2aba738030481f9e36e0effb2bd828
imdla <imdla918@gmail.com>
2025-03-17 22:36:46 +0900

    [EC-12] refactor: index 경로 추가

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layout/RootLayout";
+import NotFound from '../pages/error/NotFound';
+import Home from '../pages/home/Home';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
