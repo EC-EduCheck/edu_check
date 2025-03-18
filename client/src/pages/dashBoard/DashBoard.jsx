7b0821579d4016961e2ec89789429d1dda2774a4
imdla <imdla918@gmail.com>
2025-03-18 20:33:15 +0900

    [EC-14] feat: DashBoard 스타일 및 기능 구현

--- client/src/pages/dashBoard/DashBoard.jsx
+++ client/src/pages/dashBoard/DashBoard.jsx
@@ -3,6 +3,7 @@ import styles from "./DashBoard.module.css"
 import Tab from '../../components/tab/Tab';
 import SideBar from '../../components/sideBar/SideBar';
 import { useSelector } from 'react-redux';
+import DashBoardItem from '../../components/dashBoardItem/DashBoardItem';
 
 export default function DashBoard() {
   const currentSideBarItem = useSelector((state) => state.sideBarItem.nav);
@@ -12,7 +13,9 @@ export default function DashBoard() {
       <SideBar></SideBar>
       <div className={styles.dashBoardBox}>
         <Tab menuType={currentSideBarItem}></Tab>
-        <div className={styles.dashBoardContent}></div>
+        <div className={styles.dashBoardContent}>
+          <DashBoardItem width="100%"></DashBoardItem>
+        </div>
       </div>
     </div>
   );
