d22b603eaf361e746929e027bca154b4c861a0d1
seungyeon <seun9yeon@naver.com>
2025-03-18 17:24:29 +0900

    [EC-17] refactor: props 수정

--- client/src/components/buttons/mainButton/MainButton.jsx
+++ client/src/components/buttons/mainButton/MainButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from "./MainButton.module.css"
 
-export default function MainButton(props) {
-  const title = props.title
+export default function MainButton({title, handleClick, bgColor}) {
+
   return (
     <div>
-      <button className={styles.mainButton} title={title}>{title}</button>
+      <button className={styles.mainButton} onClick={handleClick} style={{backgroundColor : bgColor}}>{title}</button>
     </div>
   )
 }
