7a984dceda77e16cb24b1c6a00e190532f2bc37c
seungyeon <seun9yeon@naver.com>
2025-03-19 18:19:19 +0900

    [EC-21] feat: FilterButton 공통 스타일 기능 구현

--- client/src/components/buttons/filterButton/FilterButton.jsx
+++ client/src/components/buttons/filterButton/FilterButton.jsx
@@ -4,7 +4,9 @@ import style from "./FilterButton.module.css"
 export default function FilterButton({title, content, bgcolor}) {
   return (
     <div>
-    <button className={style.filterButton} style={{backgroundColor : bgcolor}}>{title}<p className={style.text}>{content}</p>
+    <button className={style.filterButton} style={{backgroundColor : bgcolor}}>
+      <p>{title}</p>
+      <p>{content}</p>
     </button>
     </div>
   )
