d021eb3a8e36bb12dd35e81c333230e6b1d9fe5d
ysc <ysc13245@gmail.com>
2025-03-19 23:06:27 +0900

    [EC-24] feat: 로그인 실패 예외 처리

--- api/src/main/java/org/example/educheck/domain/member/controller/AuthController.java
+++ api/src/main/java/org/example/educheck/domain/member/controller/AuthController.java
@@ -8,8 +8,10 @@ import org.example.educheck.domain.member.dto.LoginResponseDto;
 import org.example.educheck.domain.member.dto.SignUpRequestDto;
 import org.example.educheck.domain.member.service.AuthService;
 import org.example.educheck.global.common.dto.ApiResponse;
+import org.example.educheck.global.common.exceptin.custom.LoginValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,11 +35,14 @@ public class AuthController {
     public ResponseEntity<ApiResponse<LoginResponseDto>> login(@RequestBody @Valid LoginRequestDto requestDto,
                                                                HttpServletResponse response) {
 
-        return ResponseEntity.ok(
-                ApiResponse.ok("로그인 성공", "OK",
-                        authService.login(requestDto, response))
+        try {
+            return ResponseEntity.ok(
+                    ApiResponse.ok("로그인 성공", "OK",
+                            authService.login(requestDto, response))
 
-        );
-
+            );
+        } catch (BadCredentialsException ex) {
+            throw new LoginValidationException();
+        }
     }
 }
