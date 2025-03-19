42c90b06fa86f403a772c3ddf9d334c011fe317b
yejin <yejinoh58@gmail.com>
2025-03-20 00:06:35 +0900

    [EC-30] chore: WIP

--- api/src/main/java/org/example/educheck/domain/meetingroomreservation/service/MeetingRoomReservationService.java
+++ api/src/main/java/org/example/educheck/domain/meetingroomreservation/service/MeetingRoomReservationService.java
@@ -3,11 +3,13 @@ package org.example.educheck.domain.meetingroomreservation.service;
 import lombok.RequiredArgsConstructor;
 import org.example.educheck.domain.meetingroom.entity.MeetingRoom;
 import org.example.educheck.domain.meetingroom.repository.MeetingRoomRepository;
+import org.example.educheck.domain.meetingroomreservation.TimeSlot;
 import org.example.educheck.domain.meetingroomreservation.dto.request.MeetingRoomReservationRequestDto;
 import org.example.educheck.domain.meetingroomreservation.entity.MeetingRoomReservation;
 import org.example.educheck.domain.meetingroomreservation.repository.MeetingRoomReservationRepository;
 import org.example.educheck.domain.member.entity.Member;
 import org.example.educheck.domain.member.repository.MemberRepository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +17,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -25,6 +29,7 @@ public class MeetingRoomReservationService {
     private final MeetingRoomReservationRepository meetingRoomReservationRepository;
     private final MemberRepository memberRepository;
     private final MeetingRoomRepository meetingRoomRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Transactional
     public void createReservation(UserDetails user, Long campusId, MeetingRoomReservationRequestDto requestDto) {
@@ -38,6 +43,9 @@ public class MeetingRoomReservationService {
 
         validateReservationTime(requestDto.getStartTime(), requestDto.getEndTime());
 
+//        if(!isAvailable(meetingRoom.getId(), TimeSlot.from(requestDto))) {
+//            throw new
+//        }
 
         validateReservableTime(meetingRoom, requestDto.getStartTime(), requestDto.getEndTime());
 
@@ -88,5 +96,60 @@ public class MeetingRoomReservationService {
         if (!campusId.equals(meetingRoom.getCampusId())) {
             throw new IllegalArgumentException("해당 회의실은 캠퍼스내 회의실이 아닙니다.");
         }
+    }
+
+    private String generateSlotKey(Long roomId, LocalDate date) {
+        return String.format("mettingRoom:%d:date:%s", roomId, date.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+    }
+
+    private int calculateSlots(int startHour, int endHour, int slotDurationMinutes) {
+        int totalMinutes = (endHour - startHour) * 60;
+        return totalMinutes / slotDurationMinutes;
+
+    }
+
+    private void initDailyReservationsSlots(LocalDate date) {
+        List<MeetingRoom> roomList = meetingRoomRepository.findAll();
+
+        for (MeetingRoom room : roomList) {
+            String redisKey = generateSlotKey(room.getId(), date);
+
+            int slotsCount = calculateSlots(9, 22, 15);
+            Boolean[] slots = new Boolean[slotsCount];
+            for (int i = 0; i < slotsCount; i++) {
+                slots[i] = false;
+            }
+
+            redisTemplate.opsForValue().set(redisKey, slots);
+        }
+    }
+
+    public boolean isAvailable(Long meetingRoomId, TimeSlot timeSlot) {
+        String redisKey = generateSlotKey(meetingRoomId, timeSlot.getDate());
+        Boolean[] slots = (Boolean[]) redisTemplate.opsForValue().get(redisKey);
+
+        if (slots == null) {
+            initDailyReservationsSlots(timeSlot.getDate());
+            slots = (Boolean[]) redisTemplate.opsForValue().get(redisKey);
+        }
+
+        int startSlotIndex = calculateSlotIndex(timeSlot.getStartTime());
+        int endSlotIndex = calculateSlotIndex(timeSlot.getStartTime());
+
+        for (int i = startSlotIndex; i <= endSlotIndex; i++) {
+            if (slots[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int calculateSlotIndex(LocalTime localTime) {
+        int hour = localTime.getHour();
+        int minute = localTime.getMinute();
+
+        return (hour - 9) * 4 + (minute / 15);
+
     }
 }
--- api/src/main/java/org/example/educheck/global/common/config/RedisConfig.java
+++ api/src/main/java/org/example/educheck/global/common/config/RedisConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -33,6 +36,27 @@ public class RedisConfig {
 
         StringRedisTemplate template = new StringRedisTemplate();
         template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
+                redisHost, redisPort);
+        redisStandaloneConfiguration.setPassword(password);
+
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        connectionFactory.setValidateConnection(true);
+        connectionFactory.afterPropertiesSet();
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return template;
     }
 }
