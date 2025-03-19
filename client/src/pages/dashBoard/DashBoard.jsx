29be7013b088bee770ab5d9578f69e88ac53089e
imdla <imdla918@gmail.com>
2025-03-19 11:48:57 +0900

    [EC-23] style: buttonContent title 내용 추가

--- client/src/pages/dashBoard/DashBoard.jsx
+++ client/src/pages/dashBoard/DashBoard.jsx
@@ -4,6 +4,7 @@ import styles from './DashBoard.module.css';
 import Tab from '../../components/tab/Tab';
 import SideBar from '../../components/sideBar/SideBar';
 import DashBoardItem from '../../components/dashBoardItem/DashBoardItem';
+import RoundButton from '../../components/buttons/roundButton/RoundButton';
 
 export default function DashBoard() {
   const currentSideBarItem = useSelector((state) => state.sideBarItem.nav);
@@ -16,7 +17,10 @@ export default function DashBoard() {
 
         {/* dashBoardContent 내부에 대시보드 및 컴포넌트 사용 */}
         <div className={styles.dashBoardContent}>
-          <DashBoardItem width="100%"></DashBoardItem>
+          <DashBoardItem width="100%">
+            <RoundButton title="결석"></RoundButton>
+            <RoundButton title="조퇴"></RoundButton>
+          </DashBoardItem>
         </div>
       </div>
     </div>
