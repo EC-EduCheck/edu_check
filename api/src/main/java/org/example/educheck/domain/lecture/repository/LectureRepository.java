82a631f8d487062e0f3b99b43c6142f77aa825c9
daejungyoon <dbseowjd12@gmail.com>
2025-03-19 22:12:33 +0900

    [EC-15] feat: course id에 따른 과정 찾기 메서드 구현

--- api/src/main/java/org/example/educheck/domain/lecture/repository/LectureRepository.java
+++ api/src/main/java/org/example/educheck/domain/lecture/repository/LectureRepository.java
@@ -2,6 +2,8 @@ package org.example.educheck.domain.lecture.repository;
 
 import org.example.educheck.domain.lecture.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.time.LocalDateTime;
@@ -9,4 +11,9 @@ import java.util.Optional;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
     Optional<Lecture> findByCourseIdAndDate(Long courseId, LocalDateTime date);
+    @Query("SELECT l FROM Lecture l WHERE l.course.id = :courseId AND l.date BETWEEN :startDate AND :endDate")
+    Optional<Lecture> findByCourseIdAndDateBetween(
+            @Param("courseId") Long courseId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);
 }
