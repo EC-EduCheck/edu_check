acee2139b0cbdb7085429a98d9cf3250fe31ac11
yejin <yejinoh58@gmail.com>
2025-03-21 14:38:12 +0900

    [EC-60] feat: 특정 예약 내역 조회 API 구현

--- api/src/main/java/org/example/educheck/domain/meetingroomreservation/dto/response/MeetingRoomReservationResponseDto.java
+++ api/src/main/java/org/example/educheck/domain/meetingroomreservation/dto/response/MeetingRoomReservationResponseDto.java
@@ -2,17 +2,29 @@ package org.example.educheck.domain.meetingroomreservation.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.example.educheck.domain.meetingroomreservation.entity.MeetingRoomReservation;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
 public class MeetingRoomReservationResponseDto {
-    private String reserverId;
+    private Long reserverId;
     private String reserverName;
     private LocalDateTime startDateTime;
     private LocalDateTime endDateTime;
     private Long meetingRoomId;
     private String meetingRoomName;
+
+    public static MeetingRoomReservationResponseDto from(MeetingRoomReservation reservation) {
+        return MeetingRoomReservationResponseDto.builder()
+                .reserverId(reservation.getMember().getId())
+                .reserverName(reservation.getMember().getName())
+                .startDateTime(reservation.getStartTime())
+                .endDateTime(reservation.getEndTime())
+                .meetingRoomId(reservation.getMeetingRoom().getId())
+                .meetingRoomName(reservation.getMeetingRoom().getName())
+                .build();
+    }
 
 }
