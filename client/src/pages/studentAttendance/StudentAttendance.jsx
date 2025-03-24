e128b29e7c9989d5ecd41f152b4d99322cfce324
daejungyoon <dbseowjd12@gmail.com>
2025-03-24 16:48:23 +0900

    [EC-74] feat: 학습자 출석 현황 wip

--- client/src/pages/studentAttendance/StudentAttendance.jsx
+++ client/src/pages/studentAttendance/StudentAttendance.jsx
@@ -4,23 +4,28 @@ import DashBoardItem from '../../components/dashBoardItem/DashBoardItem';
 import DataBoard from '../../components/dataBoard/DataBoard';
 
 export default function StudentAttendance() {
+  const attendanceRatio = 56;
+  const lateCount = 3;
+  const earlyLeaveCount = 0;
+  const absenceCount = 0;
+  const cumulativeAbsenceCount = 1;
   return (
     <>
       <div className={styles.dashBoardItemDiv}>
         <div className={styles.attendanceStatistics}>
           <div className={styles.attendanceRatio}>
             <DashBoardItem width={'100%'}>
-            <p>출석률 56%</p>
+              <p className={styles.headingText}>출석률 {attendanceRatio}%</p>
             </DashBoardItem>
           </div>
           <div className={styles.attendanceCurrent}>
             <DashBoardItem width={'100%'}>
-              <p>결석 현황</p>
-              <div className={styles.AttendanceType}>
-                <DataBoard title="지각" data="0회"></DataBoard>
-                <DataBoard title="조퇴" data="0회"></DataBoard>
-                <DataBoard title="결석" data="0회"></DataBoard>
-                <DataBoard title="누적 결석" data="0회"></DataBoard>
+              <p className={styles.headingText}>결석 현황</p>
+              <div className={styles.attendanceType}>
+                <DataBoard title="지각" data={`${lateCount}회`}></DataBoard>
+                <DataBoard title="조퇴" data={`${earlyLeaveCount}회`}></DataBoard>
+                <DataBoard title="결석" data={`${absenceCount}회`}></DataBoard>
+                <DataBoard title="누적 결석" data={`${cumulativeAbsenceCount}회`}></DataBoard>
               </div>
             </DashBoardItem>
           </div>
