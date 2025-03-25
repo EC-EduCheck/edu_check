7c7d8025432d1851c6b284729d4ba33e696c6045
ysc <ysc13245@gmail.com>
2025-03-25 22:54:38 +0900

    [EC-80] fix: absenceAttendances 엔드포인트 수정

--- client/src/api/absenceAttendancesApi.js
+++ client/src/api/absenceAttendancesApi.js
@@ -3,7 +3,7 @@ import apiInstance from "./instance/apiInstance";
 export const absenceAttendancesApi = {
   getAbsenceAttendancesByCourseId: async (courseId) => {
     const response = await apiInstance.get(
-      `/api/course/${courseId}/absence-attendances`
+      `/course/${courseId}/absence-attendances`
     )
 
     return response
