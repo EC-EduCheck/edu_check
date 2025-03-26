dd6885f46e46ecaa5f2202b561e4e0ebe1416bdd
yejin <yejinoh58@gmail.com>
2025-03-26 12:25:09 +0900

    [EC-106] feat: 출석부 확인을 위한 엔티티 추가

---
+++ api/src/main/java/org/example/educheck/domain/studentCourseAttendance/entity/StudentCourseAttendance.java
@@ -0,0 +1,46 @@
+package org.example.educheck.domain.studentCourseAttendance.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import org.hibernate.annotations.Immutable;
+
+@Entity
+@Immutable
+public class StudentCourseAttendance {
+
+    @Column(name = "student_id")
+    private Long studentId;
+
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "member_name")
+    private String memberName;
+
+    @Column(name = "registration_status")
+    private String registrationStatus;
+
+    @Column(name = "course_name")
+    private String courseName;
+
+    @Column(name = "course_id")
+    private Long courseId;
+
+    @Column(name = "lecture_session")
+    private String lectureSession;
+
+    @Column(name = "lecture_date")
+    private String lectureDate;
+
+    @Column(name = "lecture_title")
+    private String lectureTitle;
+
+    @Column(name = "attendance_status")
+    private String attendanceStatus;
+
+    @Column(name = "check_in_timestamp")
+    private String checkInTimestamp;
+
+    @Column(name = "check_out_timestamp")
+    private String checkOutTimestamp;
+}
