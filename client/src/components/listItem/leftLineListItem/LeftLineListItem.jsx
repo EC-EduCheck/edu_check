f20a87a01da06a4ea0e9a892ff395313ed73a0c4
ysc <ysc13245@gmail.com>
2025-03-26 16:12:02 +0900

    [EC-80] feat: status에 따른 동적 color

--- client/src/components/listItem/leftLineListItem/LeftLineListItem.jsx
+++ client/src/components/listItem/leftLineListItem/LeftLineListItem.jsx
@@ -5,18 +5,27 @@ import MoreButton from '../../buttons/moreButton/MoreButton';
 
 // TODO : 관리자의 유고 결석 관리만 isClickable = true
 export default function LeftLineListItem({ isClickable, handleClick, status, children }) {
+  const categoryMapper = {
+    ABSENCE: '결석',
+    LATE: '지각',
+    EARLY_LEAVE: '조퇴',
+  };
+
   const bgColor = getBackgroundColor(status);
+console.log(bgColor);
 
   return (
     <div
       {...(isClickable ? { onClick: handleClick } : {})}
       className={`${styles.leftLineListItem} ${isClickable && `${styles.active}`} ${bgColor ? styles[bgColor] : ''}`}
     >
-      {children.studentName}
-      {status}
-      {/* 출석상태 */}
-      {children.attached ? '첨부' : '미첨부'}
-      {children.status ? '승인' : false ? '반려' : '미승인'}
+      
+      <p>{children.studentName}</p>
+      <p>{status}</p>
+      <p>{categoryMapper[children.category] || ''}</p>
+      <p>{children.attached ? '첨부' : '미첨부'}</p>
+      <p>{new Date(children.createdAt).toLocaleDateString()}</p>
+      <p>{children.status ? '승인' : false ? '반려' : '미승인'}</p>
 
       {!isClickable && <MoreButton></MoreButton>}
     </div>
