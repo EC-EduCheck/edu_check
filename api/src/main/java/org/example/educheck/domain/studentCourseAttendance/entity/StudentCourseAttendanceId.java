eb1d6742f0d30e50bcf5541477b79ffbe714ad4d
yejin <yejinoh58@gmail.com>
2025-03-26 15:13:12 +0900

    [EC-106] feat: embedded 복합키 구현

--- api/src/main/java/org/example/educheck/domain/studentCourseAttendance/entity/StudentCourseAttendanceId.java
+++ api/src/main/java/org/example/educheck/domain/studentCourseAttendance/entity/StudentCourseAttendanceId.java
@@ -2,9 +2,12 @@ package org.example.educheck.domain.studentCourseAttendance.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
+@NoArgsConstructor
 @Embeddable
 public class StudentCourseAttendanceId implements Serializable {
 
@@ -16,4 +19,17 @@ public class StudentCourseAttendanceId implements Serializable {
 
     @Column(name = "lecture_id")
     private Long lectureId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentCourseAttendanceId that = (StudentCourseAttendanceId) o;
+        return Objects.equals(studentId, that.studentId) && Objects.equals(courseId, that.courseId) && Objects.equals(lectureId, that.lectureId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, courseId, lectureId);
+    }
 }
