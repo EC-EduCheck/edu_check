8eab3782114ebdb1fc71c859f7e011a8c2b93d7d
ysc <ysc13245@gmail.com>
2025-03-27 00:45:10 +0900

    [EC-80] feat: 유고 결석 관리 버튼 생성

--- client/src/pages/staffAttendanceAbsence/StaffAttendanceAbsence.jsx
+++ client/src/pages/staffAttendanceAbsence/StaffAttendanceAbsence.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { act, useEffect, useState } from 'react';
 import styles from './StaffAttendanceAbsence.module.css';
 import { absenceAttendancesApi } from '../../api/absenceAttendancesApi';
 import LeftLineListItem from '../../components/listItem/leftLineListItem/LeftLineListItem';
 import { useSelector } from 'react-redux';
 import MainButton from '../../components/buttons/mainButton/MainButton';
+import { activeTitle } from '../../utils/buttonContentList';
 
 export default function StaffAttendanceAbsence() {
   const [data, setData] = useState();
@@ -20,36 +21,47 @@ export default function StaffAttendanceAbsence() {
       }
     }
     fetchData();
+    activeTitle.push('전체');
   }, []);
 
   const buttonList = ['전체', '승인', '미승인', '반려'];
-  const [selected, setSelected] = useState(buttonList[0]);
+  const [selected, setSelected] = useState('전체');
 
   const handleButtonClick = (title) => {
+    for (let i = activeTitle.length - 1; i >= 0; i--) {
+      if (buttonList.includes(activeTitle[i])) {
+        activeTitle.splice(i, 1);
+      }
+    }
+    activeTitle.push(title);
     setSelected(title);
   };
+  console.log(selected);
+  
 
   return (
     <>
-      <div className="buttonContainer">
-        {buttonList.map((title, idx) => (
-          <MainButton
-            key={idx}
-            title={title}
-            isActive={selected === title}
-            handleClick={() => handleButtonClick(title)}
-          />
-        ))}
-      </div>
-      {data?.absenceAttendances &&
-        data.absenceAttendances.map((item, idx) => (
-          <LeftLineListItem
-            isClickable={role === 'MIDDLE_ADMIN'}
-            status={item.status}
-            children={item}
-            key={idx}
-          />
-        ))}
-    </>
+    <div className="buttonContainer">
+      {buttonList.map((title, idx) => (
+        <MainButton
+          key={idx}
+          title={title}
+          isActive={activeTitle.includes(title)}
+          isEnable={true}
+          handleClick={() => handleButtonClick(title)}
+        />
+      ))}
+    </div>
+
+        {data?.absenceAttendances &&
+          data.absenceAttendances.map((item, idx) => (
+            <LeftLineListItem
+              isClickable={role === 'MIDDLE_ADMIN'}
+              status={item.status}
+              children={item}
+              key={idx}
+            />
+          ))}
+      </>
   );
 }
