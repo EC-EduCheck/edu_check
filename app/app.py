282f5f9d6c2c398d9046aefc9987a431e9564663
ysc <ysc13245@gmail.com>
2025-03-30 07:12:33 +0900

    [EC-157] feat: 출석부 엑셀 다운로드 기능 구현

--- app/app.py
+++ app/app.py
@@ -46,5 +46,21 @@ def data(student_id, course_id):
     return jsonify(df.to_dict(orient="records"))
 
 
+
+@app.route(
+    "/app/courses/<int:course_id>/students/<int:student_id>/download", methods=["GET"]
+)
+def download(student_id, course_id):
+    data = get_data_from_db(student_id, course_id)
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+        data.to_excel(writer, index=False, sheet_name="Data")
+    output.seek(0)
+
+    return send_file(
+        output, download_name=f"{student_id}_출석부.xlsx", as_attachment=True
+    )
+
+
 if __name__ == "__main__":
     app.run(debug=False)
