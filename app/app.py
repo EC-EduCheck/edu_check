3156369e855b8ab38c4008f8bf0cce314ce0c3dc
ysc <ysc13245@gmail.com>
2025-03-30 06:53:39 +0900

    [EC-157] feat: 출석부 조회 기능 구현

--- app/app.py
+++ app/app.py
@@ -20,5 +20,31 @@ dbconfig = {
 }
 
 
+cnxpool = pooling.MySQLConnectionPool(pool_name="pool", pool_size=5, **dbconfig)
+
+
+def get_data_from_db(student_id, course_id):
+
+    conn = cnxpool.get_connection()
+    try:
+        query = """
+    SELECT lecture_session, lecture_date, attendance_status
+    FROM student_course_attendance
+    WHERE student_id = %s AND course_id = %s
+"""
+        data = pd.read_sql(query, conn, params=(student_id, course_id))
+
+    finally:
+        conn.close()
+    return data
+
+
+@app.route("/app/courses/<int:course_id>/students/<int:student_id>", methods=["GET"])
+def data(student_id, course_id):
+    df = get_data_from_db(student_id, course_id)
+
+    return jsonify(df.to_dict(orient="records"))
+
+
 if __name__ == "__main__":
     app.run(debug=False)
