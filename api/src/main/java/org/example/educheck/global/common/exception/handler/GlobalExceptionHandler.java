f69fa254c5d9997b0331d4a2a4f75d3ef3b36110
yejin <yejinoh58@gmail.com>
2025-03-30 23:54:37 +0900

    [EC-176] feat: 타입 불일치에 대한 예외 처리
    - 컨트롤러 API 타입 불일치 MethodArgumentTypeMismatchException 예외 처리

--- api/src/main/java/org/example/educheck/global/common/exception/handler/GlobalExceptionHandler.java
+++ api/src/main/java/org/example/educheck/global/common/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.example.educheck.global.common.dto.ApiResponse;
 import org.example.educheck.global.common.exception.ErrorCode;
 import org.example.educheck.global.common.exception.custom.LoginValidationException;
 import org.example.educheck.global.common.exception.custom.common.GlobalException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.List;
@@ -89,5 +91,13 @@ public class GlobalExceptionHandler {
                 .status(ErrorCode.INVALID_INPUT.getStatus())
                 .body(ApiResponse.error(ErrorCode.INVALID_INPUT.getMessage(),
                         ErrorCode.INVALID_INPUT.getCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(String.format("잘못된 요청: '%s' 값은 %s 타입이어야 합니다.", ex.getName(), ex.getRequiredType().getSimpleName()), ErrorCode.INVALID_INPUT.getCode())
+                );
     }
 }
