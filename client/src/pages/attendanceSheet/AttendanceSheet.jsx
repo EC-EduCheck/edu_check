88d2176403310af31fdc14c8159b77966d6261dc
ysc13245 <ysc13245@gmail.com>
2025-04-01 05:40:54 +0900

    [EC-198] feat: 스타일 wip

--- client/src/pages/attendanceSheet/AttendanceSheet.jsx
+++ client/src/pages/attendanceSheet/AttendanceSheet.jsx
@@ -1,38 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { attendanceSheetApi } from '../../api/attendanceSheetApi';
-import store from '../../store/store';
 import { useSelector } from 'react-redux';
+import styles from './AttendanceSheet.module.css';
 
 export default function AttendanceSheet() {
   const [sheetData, setSheetData] = useState([]);
 
   const courseId = useSelector((state) => state.auth.user.courseId);
+  const courseName = useSelector((state) => state.auth.user.courseName);
   const memberId = useSelector((state) => state.auth.user.memberId);
+  const name = useSelector((state) => state.auth.user.name);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await attendanceSheetApi.getStudentAttendanceSheet(courseId, memberId);
       setSheetData(response.data);
     };
-    fetchData();
-  }, [courseId, memberId]);
+    courseId && memberId && fetchData();
+  }, [courseId, memberId, name, courseName]);
   console.log(sheetData);
 
   return (
-    <table border="1" style={{ borderCollapse: 'collapse', width: '100%' }}>
-      <thead>
+    <table className={styles.table}>
+      <thead className={styles.thead}>
         <tr>
-          <th>회차</th>
-          <th>날짜</th>
-          <th>출석 상태</th>
+          <th className={styles.th}>회차</th>
+          <th className={styles.th}>날짜</th>
+          <th className={styles.th}>출석 상태</th>
         </tr>
       </thead>
       <tbody>
         {sheetData.map((item, idx) => (
           <tr key={idx}>
-            <td>{item['회차']}</td>
-            <td>{item['날짜']}</td>
-            <td>{item['출석 상태']}</td>
+            <td className={styles.td}>{item['회차']}</td>
+            <td className={styles.td}>{item['날짜']}</td>
+            <td className={styles.td}>{item['출석 상태']}</td>
           </tr>
         ))}
       </tbody>
