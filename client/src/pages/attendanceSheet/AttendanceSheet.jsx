94555779bb9d762880e631de8dadfc47fb68828f
ysc13245 <ysc13245@gmail.com>
2025-04-01 04:49:38 +0900

    [EC-198] feat: 표 생성

--- client/src/pages/attendanceSheet/AttendanceSheet.jsx
+++ client/src/pages/attendanceSheet/AttendanceSheet.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { attendanceSheetApi } from '../../api/attendanceSheetApi';
+import store from '../../store/store';
+import { useSelector } from 'react-redux';
 
 export default function AttendanceSheet() {
+  const [sheetData, setSheetData] = useState([]);
+
+  const courseId = useSelector((state) => state.auth.user.courseId);
+  const memberId = useSelector((state) => state.auth.user.memberId);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await attendanceSheetApi.getStudentAttendanceSheet(courseId, memberId);
+      setSheetData(response.data);
+    };
+    fetchData();
+  }, [courseId, memberId]);
+  console.log(sheetData);
+
   return (
-    <div>AttendanceSheet</div>
-  )
+    <table border="1" style={{ borderCollapse: 'collapse', width: '100%' }}>
+      <thead>
+        <tr>
+          <th>회차</th>
+          <th>날짜</th>
+          <th>출석 상태</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sheetData.map((item, idx) => (
+          <tr key={idx}>
+            <td>{item['회차']}</td>
+            <td>{item['날짜']}</td>
+            <td>{item['출석 상태']}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
