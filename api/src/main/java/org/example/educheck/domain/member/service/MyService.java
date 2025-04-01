5b89dd9f4f7fcc062298b7c18716bfcf1e56ac8b
yejin <yejinoh58@gmail.com>
2025-04-02 00:29:51 +0900

    [EC-205] feat: 개인정보 조회 API 로그

--- api/src/main/java/org/example/educheck/domain/member/service/MyService.java
+++ api/src/main/java/org/example/educheck/domain/member/service/MyService.java
@@ -1,12 +1,14 @@
 package org.example.educheck.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.educheck.domain.member.dto.response.MyProfileResponseDto;
 import org.example.educheck.domain.member.entity.Member;
 import org.example.educheck.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -16,6 +18,7 @@ public class MyService {
 
 
     public MyProfileResponseDto getMyProfile(Member member) {
+        log.info("getMyProfile", member.getId());
         return memberRepository.findMyProfileDtoById(member.getId());
     }
 }
