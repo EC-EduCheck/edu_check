604adb0ac3626602c487b4acaa0bc716d415482a
yejin <yejinoh58@gmail.com>
2025-04-03 13:41:08 +0900

    [EC-224] feat: 유고결석 신청 API 연동

--- client/src/pages/studentAttendanceAbsence/StudentAttendanceAbsence.jsx
+++ client/src/pages/studentAttendanceAbsence/StudentAttendanceAbsence.jsx
@@ -8,9 +8,11 @@ import NewInputBox from '../../components/inputBox/newInputBox/NewInputBox';
 import Modal from '../../components/modal/Modal';
 import 'react-datepicker/dist/react-datepicker.css';
 import DatePicker from 'react-datepicker';
-import { ko } from 'date-fns/locale';
+import { fi, ko } from 'date-fns/locale';
 import { absenceAttendancesApi } from '../../api/absenceAttendancesApi';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { URL_PATHS } from '../../constants/urlPaths';
 
 export default function StudentAttendanceAbsence() {
   const courseId = useSelector((state) => state.auth.user.courseId);
@@ -22,56 +24,63 @@ export default function StudentAttendanceAbsence() {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [reason, setReason] = useState('');
-  const [file, setFile] = useState(null);
   const [currentItem, setCurrentItem] = useState(null);
   const fileInputRef = useRef(null);
   const [files, setFiles] = useState(null);
   const [fileNames, setFileNames] = useState('');
+  const navigate = useNavigate();
   const [uploadData, setUploadData] = useState({
     category: '',
     startDate: '',
     endDate: '',
-    // files: [],
     reason: '',
   });
 
-  useEffect(() => {
-    const fetchAbsenceList = async (courseId) => {
-      try {
-        setLoading(true);
-        const response = await absenceAttendancesApi.getAbsenceAttendanceListByStudent(courseId);
+  const resetFormFields = () => {
+    setUploadData({
+      category: categoryMap[isActiveIndex], 
+      startDate: '',
+      endDate: '',
+      reason: '',
+    });
+    setFiles(null);
+    setFileNames('');
 
-        if (response.data && response.data.data && response.data.data.content) {
-          setAbsenceList(response.data.data.content);
-        } else {
-          setAbsenceList([]);
-        }
-        setLoading(false);
-      } catch (err) {
-        console.error('유고 결석 데이터 조회 실패:', err);
-        setError('유고 결석 데이터를 불러오는데 실패했습니다.');
-        setLoading(false);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = '';
+    }
+  };
+
+  const fetchAbsenceList = async (courseId) => {
+    try {
+      setLoading(true);
+      const response = await absenceAttendancesApi.getAbsenceAttendanceListByStudent(courseId);
+
+      if (response.data && response.data.data && response.data.data.content) {
+        setAbsenceList(response.data.data.content);
+      } else {
+        setAbsenceList([]);
       }
-    };
+      setLoading(false);
+    } catch (err) {
+      console.error('유고 결석 데이터 조회 실패:', err);
+      setError('유고 결석 데이터를 불러오는데 실패했습니다.');
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     if (courseId) {
       fetchAbsenceList(courseId);
     }
   }, [courseId]);
 
   const handleFileChange = (e) => {
-    //이벤트 객체를 통해서 선택된 파일 정보에 접근
-    //선택한 파일들을 가져오는데, 실제 객체가 아니라 map 돌리기 위해 배여로 변환
     const selectedFiles = Array.from(e.target.files);
-    //변환된 파일 이름 처리하기, 다중이라 사용자에게 어떤 파일이 선택되었는지 보여줘야 함
     const names = selectedFiles.map((file) => file.name).join(',');
 
-    //서버로 전송하기 위한 파일객체 상태 관리
     setFiles(selectedFiles);
-    console.log('업로드한 파일 목록', selectedFiles);
-
     setFileNames(names);
-    console.log('보여줄 파일들', fileNames);
   };
 
   const handleTagChange = (item) => {
@@ -128,14 +137,12 @@ export default function StudentAttendanceAbsence() {
       reason: reason,
     };
 
-    // 리스트 업데이트 (map 함수 사용)
     setAbsenceList((prevList) =>
       prevList.map((item) =>
         item.absenceAttendanceId === currentItem.absenceAttendanceId ? updatedItem : item,
       ),
     );
 
-    // 모달 닫기 및 상태 초기화
     setOpenModal(false);
     setCurrentItem(null);
 
@@ -180,61 +187,18 @@ export default function StudentAttendanceAbsence() {
     </>
   );
 
-  // const handleOnChange = (event) => {
-  //   if (event && event.target) {
-  //     const { name, value } = event.target;
-
-  //     console.log(name);
-  //     console.log(value);
-
-  //     setUploadData((prev) => ({
-  //       ...prev,
-  //       [name]: value,
-  //     }));
-
-  //     console.log(uploadData);
-  //     //Date Picker인 경우
-  //   } else if (event instanceof Date) {
-  //     console.log(event);
-  //     // setUploadData((prev) => ({
-  //     //   ...prev,
-  //     //   [value] = name,
-  //     // })
-  //   }
-  // };
-
   const handleOnChange = (name, value) => {
-    if (value instanceof Date) {
-      console.log('선택된 날짜:', value);
-
-      setUploadData((prev) => ({
-        ...prev,
-        [name]: value, // 'startDate'에 날짜 값 저장
-      }));
-    } else if (value && value.target) {
-      const { name: eventName, value: eventValue } = value.target;
-
-      console.log('입력 변경:', eventName, eventValue);
-
-      setUploadData((prev) => ({
-        ...prev,
-        [eventName || name]: eventValue,
-      }));
-    }
+    setUploadData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    //FormData 객체 생성
     const formData = new FormData();
 
-    //FormData객체에 다중파일 첨부하기
-    files.forEach((file) => {
-      formData.append('file', file);
-    });
-
-    // 날짜 포맷 함수
     const formatDate = (date) => {
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, '0');
@@ -242,17 +206,31 @@ export default function StudentAttendanceAbsence() {
       return `${year}-${month}-${day}`;
     };
 
-    // requestBody 내용 첨부
     const jsonData = {
       startDate: formatDate(uploadData.startDate),
       endDate: formatDate(uploadData.endDate),
-      category: categoryMap[isActiveIndex],
+      category: uploadData.category,
       reason: uploadData.reason,
     };
 
-    formData.append('data', JSON.stringify(jsonData));
+    formData.append('data', new Blob([JSON.stringify(jsonData)], { type: 'application/json' }));
 
-    //API 호출
+    files.forEach((file) => {
+      formData.append('files', file);
+    });
+
+    try {
+      const response = await absenceAttendancesApi.submitAbsenceAttendance(courseId, formData);
+      if (response.status === 200 || response.status === 201) {
+        alert('유고 결석 신청이 완료되었습니다.');
+        resetFormFields();
+        if (courseId) {
+          fetchAbsenceList(courseId);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const list = ['결석', '조퇴', '지각'];
@@ -368,7 +346,7 @@ export default function StudentAttendanceAbsence() {
             innerRef={fileInputRef}
           />
           <div className={styles.editReason}>
-            <NewInputBox label="사유" value={reason} onChange={(e) => setReason(e.target.value)} />
+            <NewInputBox label="사유" value={reason} onChange={handleOnChange} />
           </div>
         </div>
       </div>
@@ -379,7 +357,7 @@ export default function StudentAttendanceAbsence() {
     <>
       <div className={styles.LeftLineListItemDisplay}>
         <div className={styles.absenceLeftLineListItem}>
-          <p className="subTitle">신청 내역</p>
+          <p className="subTitle">신청 내역(페이지네이션 필요)</p>
           <div className={styles.absenceAttendanceList}>{absenceListItems}</div>
         </div>
 
@@ -418,7 +396,7 @@ export default function StudentAttendanceAbsence() {
                   label="서류"
                   title="파일 선택 또는 끌어놓기..."
                   type="file"
-                  multiple={true}
+                  isMultiple={true}
                   onChange={handleFileChange}
                   innerRef={fileInputRef}
                 />
@@ -427,8 +405,8 @@ export default function StudentAttendanceAbsence() {
                     label="사유"
                     name="reason"
                     type="text"
-                    value={reason || uploadData.reason}
-                    onChange={handleOnChange}
+                    value={uploadData.reason}
+                    onChange={(event) => handleOnChange('reason', event.target.value)}
                   />
                 </div>
               </div>
